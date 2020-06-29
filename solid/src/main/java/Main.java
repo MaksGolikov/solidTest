305d92d0395315dc47a562a6e5091cf9caf6a291
MaksGolikov <maksim.golikov01@gmail.com>
2020-06-29 20:08:13 +0300

    first commit without norm structure

---
+++ solid/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Позвать кота есть пищу людского приготовления");
+        System.out.println("налить воды");
+        System.out.println("насыпать жрать");
+        System.out.println("кот невидит под собой тарелку, потыкать в миску мордой");
+        System.out.println("кот ест");
+        System.out.println("убрать за котом");
+    }
+}
